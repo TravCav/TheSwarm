44b581a198e728a4c66fec746f79760a775696d9
TravCav <xurrux@gmail.com>
2019-08-12 00:51:14 -0400

    show info

--- processDots.js
+++ processDots.js
@@ -61,44 +61,6 @@ function CopyDot(dotIndex, copyDot, offspring) {
   population.dots[dotIndex].generation++;
 }
 
-function DoTheThings() {
-  centerX = ctx.canvas.width / 2;
-  centerY = ctx.canvas.height / 2;
-
-  let totalEnergy = 0;
-  population.data.oldestAge = 0;
-  population.data.mostEnergy = 0;
-  population.data.mostChildren = 0;
-
-  for (let i = 0; i < population.dots.length; i++) {
-    totalEnergy += population.dots[i].energy;
-    population.dots[i].CheckDots(population);
-
-    population.dots[i].DoMovement(centerX, centerY);
-
-    if (population.dots[i].energy > population.data.mostEnergy) {
-      population.data.mostEnergyIndex = i;
-      population.data.mostEnergy = population.dots[i].energy;
-    }
-
-    if (population.dots[i].children > population.data.mostChildren) {
-      population.data.mostChildrenIndex = i;
-      population.data.mostChildren = population.dots[i].children;
-    }
-
-    if (population.dots[i].age > population.data.oldestAge) {
-      population.data.oldestAgeIndex = i;
-      population.data.oldestAge = population.dots[i].age;
-    }
-  }
-
-  CheckForDeaths();
-
-  if (population.dots.length < upperLimit && fps > 40) {
-    AddDots(1);
-  }
-}
-
 function CheckForDeaths() {
   for (let dotIndex = 0; dotIndex < population.dots.length; dotIndex++) {
     if (population.dots[dotIndex].CheckDeath() === true) {
@@ -137,11 +99,95 @@ function CheckForDeaths() {
   }
 }
 
+function DoTheThings() {
+  centerX = ctx.canvas.width / 2;
+  centerY = ctx.canvas.height / 2;
+
+  let totalEnergy = 0;
+  population.data.oldestAge = 0;
+  population.data.mostEnergy = 0;
+  population.data.mostChildren = 0;
+
+  for (let i = 0; i < population.dots.length; i++) {
+    totalEnergy += population.dots[i].energy;
+    population.dots[i].CheckDots(population);
+
+    population.dots[i].DoMovement(centerX, centerY);
+
+    if (population.dots[i].energy > population.data.mostEnergy) {
+      population.data.mostEnergyIndex = i;
+      population.data.mostEnergy = population.dots[i].energy;
+    }
+
+    if (population.dots[i].children > population.data.mostChildren) {
+      population.data.mostChildrenIndex = i;
+      population.data.mostChildren = population.dots[i].children;
+    }
+
+    if (population.dots[i].age > population.data.oldestAge) {
+      population.data.oldestAgeIndex = i;
+      population.data.oldestAge = population.dots[i].age;
+    }
+  }
+
+  CheckForDeaths();
+
+  if (population.dots.length < upperLimit && fps > 40) {
+    AddDots(1);
+  }
+
+  DrawGrid();
+
+}
+
+
+function DrawBrain(dotIndex, offset) {
+  const scale = 1.5;
+  const squareSize = scale * 5;
+  const brainSize = 100 * scale;
+
+  const dot = population.dots[dotIndex];
+  const brain = dot.brain;
+  const layerSize = brainSize / brain.layers.length;
+  const lastLayer = dot.brain.layers[brain.layers.length - 1];
+
+  let xoffset = 100 + Math.floor(brainSize + (squareSize * 2));
+  const yoffset = Math.floor(offset + (brainSize / 2));
+  const dSize = Math.floor(squareSize * 3);
+
+  for (let layerIndex = 1; layerIndex < brain.layers.length - 1; layerIndex++) {
+    const layer = brain.layers[layerIndex];
+    const neuronSize = brainSize / layer.length;
+    for (let neuronIndex = 0; neuronIndex < layer.length; neuronIndex++) {
+      const neuronValue = layer[neuronIndex].value;
+      let upshift = (brainSize - ((squareSize * layer.length))) / (scale * layer.length);
+      PlaceValueSquare(100 + Math.floor(layerIndex * layerSize), Math.floor((1 + neuronIndex) * neuronSize + offset - upshift), dot.color, neuronValue, squareSize);
+    }
+  }
+
+  PlaceValueSquare(xoffset - dSize, yoffset - dSize, dot.color, lastLayer[0].value, squareSize);
+  PlaceValueSquare(xoffset, yoffset - dSize, dot.color, lastLayer[1].value, squareSize);
+  PlaceValueSquare(xoffset + dSize, yoffset - dSize, dot.color, lastLayer[2].value, squareSize);
+
+  PlaceValueSquare(xoffset - dSize, yoffset, dot.color, lastLayer[3].value, squareSize);
+  PlaceValueSquare(xoffset + dSize, yoffset, dot.color, lastLayer[4].value, squareSize);
+
+  PlaceValueSquare(xoffset - dSize, yoffset + dSize, dot.color, lastLayer[5].value, squareSize);
+  PlaceValueSquare(xoffset, yoffset + dSize, dot.color, lastLayer[6].value, squareSize);
+  PlaceValueSquare(xoffset + dSize, yoffset + dSize, dot.color, lastLayer[7].value, squareSize);
+
+  PlaceValueSquare(xoffset, yoffset + dSize + dSize, dot.color, lastLayer[8].value, squareSize);
+
+  const xVector = (lastLayer[0].value - lastLayer[1].value) * 3;
+  const yVector = (lastLayer[2].value - lastLayer[3].value) * 3;
+
+  PlaceSquare(Math.floor(dot.vector.x + xoffset), Math.floor(dot.vector.y + yoffset), dot.color, squareSize);
+
+}
+
 function DrawGrid() {
   ctx.canvas.width = window.innerWidth;
   ctx.canvas.height = window.innerHeight;
-
-  DoTheThings();
 
   // clear screen
   pixels = ctx.createImageData(ctx.canvas.width, ctx.canvas.height);
@@ -173,7 +219,7 @@ function DrawGrid() {
     }
   }
 
-  DrawBrain(population.data.oldestAgeIndex, 30);
+  DrawBrain(population.data.oldestAgeIndex, 20);
   DrawBrain(population.data.mostChildrenIndex, 260);
 
   ctx.putImageData(pixels, 0, 0);
@@ -185,27 +231,36 @@ function DrawGrid() {
   times.push(now);
   fps = times.length;
 
+  // list details
   ctx.fillStyle = "white";
-  ctx.fillText("fps: " + fps + ", DotCount: " + population.dots.length, 20, 15);
+  ctx.fillText("fps: " + fps + ", DotCount: " + population.dots.length, 10, 15);
 
-  ctx.fillStyle = "white";
-  ctx.fillText("oldest: " + population.data.oldestAgeIndex + " - " + population.dots[population.data.oldestAgeIndex].age + " - " + population.dots[population.data.oldestAgeIndex].generation + " - " + population.dots[population.data.oldestAgeIndex].energy.toFixed(2), 20, 30);
-
-  ctx.fillStyle = "lightgreen";
-  ctx.fillText("most prolific: " + population.data.mostChildrenIndex + " - " + population.dots[population.data.mostChildrenIndex].dotsEaten + " - " + population.dots[population.data.mostChildrenIndex].children + " - " + population.dots[population.data.mostChildrenIndex].generation + " - " + population.dots[population.data.mostChildrenIndex].energy.toFixed(2), 20, 260);
+  ShowInfo(population.data.oldestAgeIndex, 30, "white");
+  ShowInfo(population.data.mostChildrenIndex, 270, "lightgreen");
 
   ctx.stroke();
 
-
-  //ListDetails();
   CircleDot(population.data.oldestAgeIndex, "white", 25);
   CircleDot(population.data.mostChildrenIndex, "green", 20);
 
   setTimeout(function () {
-    DrawGrid();
+    DoTheThings();
   }, 1);
 
   return;
+}
+
+function ShowInfo(dotIndex, yOffset, color) {
+  ctx.fillStyle = color;
+  ctx.fillText("Oldest", 10, yOffset += 10);
+  ctx.fillText("Dot: " + dotIndex, 10, yOffset += 10);
+  ctx.fillText("Gen: " + population.dots[dotIndex].generation, 10, yOffset += 10);
+  ctx.fillText("Age: " + population.dots[dotIndex].age, 10, yOffset += 10);
+  ctx.fillText("Children: " + population.dots[dotIndex].children, 10, yOffset += 10);
+  ctx.fillText("Energy: " + population.dots[dotIndex].energy.toFixed(2), 10, yOffset += 10);
+  ctx.fillText("Eaten: " + population.dots[dotIndex].dotsEaten, 10, yOffset += 10);
+  ctx.fillText("Energy: " + population.dots[dotIndex].energy.toFixed(2), 10, yOffset += 10);
+  //// ctx.fillText("Test  : 100000", 10, yOffset+=10);
 }
 
 function ListDetails() {
@@ -226,48 +281,6 @@ function CircleDot(dotIndex, color, size) {
   ctx.stroke();
 }
 
-function DrawBrain(dotIndex, offset) {
-  const scale = 1.5;
-  const squareSize = scale * 5;
-  const brainSize = 100 * scale;
-
-  const dot = population.dots[dotIndex];
-  const brain = dot.brain;
-  const layerSize = brainSize / brain.layers.length;
-  for (let layerIndex = 1; layerIndex < brain.layers.length - 1; layerIndex++) {
-    const layer = brain.layers[layerIndex];
-    const neuronSize = brainSize / layer.length;
-    for (let neuronIndex = 0; neuronIndex < layer.length; neuronIndex++) {
-      const neuronValue = layer[neuronIndex].value;
-      let upshift = (brainSize - ((squareSize * layer.length))) / (scale * layer.length);
-      PlaceValueSquare(Math.floor(layerIndex * layerSize), Math.floor((1 + neuronIndex) * neuronSize + offset - upshift), dot.color, neuronValue, squareSize);
-    }
-  }
-
-  const lastLayer = dot.brain.layers[brain.layers.length - 1];
-
-  const xoffset = Math.floor(brainSize + (squareSize * 2));
-  const yoffset = Math.floor(offset + (brainSize / 2));
-  const dSize = Math.floor(squareSize * 3);
-  PlaceValueSquare(xoffset - dSize, yoffset - dSize, dot.color, lastLayer[0].value, squareSize);
-  PlaceValueSquare(xoffset, yoffset - dSize, dot.color, lastLayer[1].value, squareSize);
-  PlaceValueSquare(xoffset + dSize, yoffset - dSize, dot.color, lastLayer[2].value, squareSize);
-
-  PlaceValueSquare(xoffset - dSize, yoffset, dot.color, lastLayer[3].value, squareSize);
-  PlaceValueSquare(xoffset + dSize, yoffset, dot.color, lastLayer[4].value, squareSize);
-
-  PlaceValueSquare(xoffset - dSize, yoffset + dSize, dot.color, lastLayer[5].value, squareSize);
-  PlaceValueSquare(xoffset, yoffset + dSize, dot.color, lastLayer[6].value, squareSize);
-  PlaceValueSquare(xoffset + dSize, yoffset + dSize, dot.color, lastLayer[7].value, squareSize);
-
-  PlaceValueSquare(xoffset, yoffset + dSize + dSize, dot.color, lastLayer[8].value, squareSize);
-
-  const xVector = (lastLayer[0].value - lastLayer[1].value) * 3;
-  const yVector = (lastLayer[2].value - lastLayer[3].value) * 3;
-
-  PlaceSquare(Math.floor(dot.vector.x + xoffset), Math.floor(dot.vector.y + yoffset), dot.color, squareSize);
-
-}
 
 function PlacePixel(x, y, color, d) {
   const index = (x + y * ctx.canvas.width) * 4;
@@ -317,4 +330,5 @@ AddDots(100);
 for (let i = 0; i < 120; i++) {
   times.push(performance.now());
 }
-DrawGrid();
+
+DoTheThings();
